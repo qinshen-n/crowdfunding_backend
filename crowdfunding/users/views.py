9c25868b5d029a56575cc214a584f5695b87b9ed
QIN SHEN <sharondaisy62@gmail.com>
2025-08-24 00:21:35 +0800

    updated token authentication

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 from django.http import Http404
 from .models import CustomUser
 from .serializers import CustomUserSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 class CustomUserList(APIView):
     def get(self, request):
@@ -38,3 +40,20 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk) # call the get_object helper method to get the data
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
+    
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class (
+           data=request.data,
+           context={'request': request} 
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email
+        })
